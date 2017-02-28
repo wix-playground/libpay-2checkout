93708046cc44750902d44cee316748123aad6d26
evgenyk <evgenyk@wix.com>
2017-02-28 18:48:29 +0200

    Implemented a faster HtmlTokenizer using ui4j

--- libpay-2checkout-htmltokenizer/src/main/scala/com/wix/pay/twocheckout/tokenization/html/HtmlTokenizer.scala
+++ libpay-2checkout-htmltokenizer/src/main/scala/com/wix/pay/twocheckout/tokenization/html/HtmlTokenizer.scala
@@ -2,7 +2,8 @@ package com.wix.pay.twocheckout.tokenization.html
 
 import java.net.URLEncoder
 
-import com.gargoylesoftware.htmlunit.{AlertHandler, Page, WebClient}
+import com.ui4j.api.browser.{BrowserFactory, PageConfiguration}
+import com.ui4j.api.dialog.{AlertHandler, DialogEvent}
 import com.wix.pay.creditcard.CreditCard
 import com.wix.pay.twocheckout.model.Environments
 import com.wix.pay.twocheckout.model.html.{Error, TokenizeResponse}
@@ -10,6 +11,8 @@ import com.wix.pay.twocheckout.tokenization.TwocheckoutTokenizer
 import org.json4s.DefaultFormats
 import org.json4s.native.Serialization
 
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Promise}
 import scala.util.Try
 
 object JavascriptSdkUrls {
@@ -20,54 +23,55 @@ object JavascriptSdkUrls {
   * [[TwocheckoutTokenizer]] that uses 2checkout's official JavaScript SDK inside a headless browser.
   */
 class HtmlTokenizer(jsSdkUrl: String = JavascriptSdkUrls.production,
-                    environment: String = Environments.production) extends TwocheckoutTokenizer {
-  override def tokenize(sellerId: String, publishableKey: String, card: CreditCard): Try[String] = {
-    Try {
-      val webClient = new WebClient
-      webClient.getOptions.setCssEnabled(false)
-      webClient.getJavaScriptEngine.getContextFactory.enterContext().setOptimizationLevel(9)
+                    environment: String = Environments.production) extends TwocheckoutTokenizer with AutoCloseable {
+  private val browser = BrowserFactory.getWebKit
 
-      try {
-        var alertMessage: Option[String] = None
-        webClient.setAlertHandler(new AlertHandler {
-          override def handleAlert(page: Page, s: String): Unit = {
-            alertMessage = Option(s)
-          }
-        })
+  override def tokenize(sellerId: String, publishableKey: String, card: CreditCard): Try[String] = Try {
+    val alertMessage: Promise[String] = Promise()
 
-        val tokenizeHtml = HtmlTokenizer.createTokenizeHtml(
-          jsSdkUrl = jsSdkUrl,
-          environment = environment,
-          sellerId = sellerId,
-          publishableKey = publishableKey,
-          card = card
-        )
+    val tokenizeHtml = HtmlTokenizer.createTokenizeHtml(
+      jsSdkUrl = jsSdkUrl,
+      environment = environment,
+      sellerId = sellerId,
+      publishableKey = publishableKey,
+      card = card
+    )
 
-        webClient.getPage(s"data:text/html,${URLEncoder.encode(tokenizeHtml, "UTF-8")}")
-
-        val responseJson = alertMessage.get
-        val response = HtmlTokenizer.parseHtmlTokenizerResponseJson(responseJson)
-        (response.error, response.value) match {
-          case (Some(error), _) =>
-            throw new RuntimeException(s"${error.errorCode}|${error.errorMsg}")
-          case (None, Some(value)) =>
-            value.response.token.token
-          case _ => throw new IllegalStateException(s"Tokenize HTML returned unexpected response format: $responseJson")
-        }
-
-      } finally {
-        webClient.close()
+    val configuration = new PageConfiguration()
+    configuration.setAlertHandler(new AlertHandler() {
+      override def handle(event: DialogEvent) = {
+        alertMessage.success(event.getMessage)
       }
+    })
+
+    // URLEncoder encodes as www-form-urlencoded, where space is '+' and not '%20'
+    val data = URLEncoder.encode(tokenizeHtml, "UTF-8").replace("+", "%20")
+    val page = browser.navigate(s"data:text/html;charset=utf-8,$data", configuration)
+
+    try {
+      val responseJson = Await.result(alertMessage.future, atMost = Duration("10s"))
+      val response = HtmlTokenizer.parseHtmlTokenizerResponseJson(responseJson)
+      (response.error, response.value) match {
+        case (Some(error), _) =>
+          throw new RuntimeException(s"${error.errorCode}|${error.errorMsg}")
+        case (None, Some(value)) =>
+          value.response.token.token
+        case _ => throw new IllegalStateException(s"Tokenize HTML returned unexpected response format: $responseJson")
+      }
+    } finally {
+      page.close()
     }
   }
+
+  override def close(): Unit = browser.shutdown()
 }
 
 private object HtmlTokenizer {
   private implicit val formats = DefaultFormats
 
   /**
-    * @param value   Success value (None on error).
-    * @param error   Error value (None on success).
+    * @param value Success value (None on error).
+    * @param error Error value (None on success).
     */
   case class HtmlTokenizerResponse(value: Option[TokenizeResponse], error: Option[Error])
 
